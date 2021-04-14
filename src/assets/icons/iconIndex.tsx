b50c405269771423f418dfd164b3b3f782ad65ae
Mateus Berlitz <wobetoberlitz@gmail.com>
2021-04-14 12:19:35 -0300

    Input e SVG resolvidos

--- src/assets/icons/iconIndex.tsx
+++ src/assets/icons/iconIndex.tsx
@@ -1,6 +1,10 @@
-import react from 'react';
+interface SVGProps{
+    bg?: string;
+    color?: string;
+    size?: number;
+}
 
-export function AlertInfoIcon(){
+export function AlertInfoIcon({color, size, bg = 'none', ...rest} : SVGProps){
     return(
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="11" stroke="#14142B" stroke-width="2"/>
@@ -10,16 +14,16 @@ export function AlertInfoIcon(){
     )
 }
 
-export function MailIcon(){
+export function MailIcon({color = "000000", size, bg = 'none', ...rest} : SVGProps){
     return(
-        <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 6C1 4.89543 1.89543 4 3 4H23C24.1046 4 25 4.89543 25 6V19C25 20.1046 24.1046 21 23 21H3C1.89543 21 1 20.1046 1 19V6Z" stroke="#14142B" stroke-width="2" stroke-linejoin="round"/>
-            <path d="M2.42131 5.30287C1.91709 4.84067 2.24409 4 2.9281 4H23.0719C23.7559 4 24.0829 4.84067 23.5787 5.30287L15.0272 13.1418C13.8802 14.1931 12.1198 14.1931 10.9728 13.1418L2.42131 5.30287Z" stroke="#14142B" stroke-width="2" stroke-linejoin="round"/>
+        <svg width="120" height="120" viewBox="0 0 26 24" fill={bg} xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 6C1 4.89543 1.89543 4 3 4H23C24.1046 4 25 4.89543 25 6V19C25 20.1046 24.1046 21 23 21H3C1.89543 21 1 20.1046 1 19V6Z" stroke={color} stroke-width="2" stroke-linejoin="round"/>
+            <path d="M2.42131 5.30287C1.91709 4.84067 2.24409 4 2.9281 4H23.0719C23.7559 4 24.0829 4.84067 23.5787 5.30287L15.0272 13.1418C13.8802 14.1931 12.1198 14.1931 10.9728 13.1418L2.42131 5.30287Z" stroke={color} stroke-width="2" stroke-linejoin="round"/>
         </svg>
     )
 }
 
-export function ViewIcon(){
+export function ViewIcon({color, size, bg = 'none', ...rest} : SVGProps){
     return(
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1.33497 13.2561C0.888345 12.4782 0.888342 11.522 1.33497 10.7441C3.68496 6.65097 7.44378 4 11.6798 4C15.9158 4 19.6746 6.65094 22.0246 10.744C22.4712 11.5219 22.4712 12.4781 22.0246 13.256C19.6746 17.3491 15.9158 20 11.6798 20C7.44377 20 3.68497 17.3491 1.33497 13.2561Z" stroke="#14142B" stroke-width="2"/>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,29 @@
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Img } from "@chakra-ui/image";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
-import { Flex, Heading } from "@chakra-ui/layout";
+import { Flex, Heading, Stack } from "@chakra-ui/layout";
 
-import { MailIcon } from '../assets/icons/';
+//import MailIcon from '../assets/icons/Mail.svg';
+import { ReactComponent as MailIcon } from '../assets/icons/Mail.svg';
 
 export default function Login(){
     return(
         <Flex w="100vw" h="100vh" align="center" justify="center">
-            <Flex as="form" w="100%" p="8" maxW={400} bg="white" borderRadius="24" boxShadow="lg" flexDir="column">
+            <Stack as="form" spacing="30" w="100%" p="8" maxW={400} bg="white" borderRadius="24" boxShadow="lg" flexDir="column">
                 <Heading>Login</Heading>
 
                 <FormControl pos="relative">
-                    <FormLabel pos="absolute" left="5" top="3" zIndex="2" fontSize="14" fontWeight="600" color="gray.600" _groupFocus={ {top: '2', fontSize:'11'}}>E-mail</FormLabel>
-
+                    {/* <FormLabel pos="absolute" left="45" top="3" zIndex="2" fontSize="14" fontWeight="600" color="gray.600" _groupFocus={ {top: '2', fontSize:'11'}}>E-mail</FormLabel> */}
+                    <FormLabel pos="absolute" left="62px" top="2" zIndex="2" fontSize="10" fontWeight="600" color="gray.600" _groupFocus={ {top: '2', fontSize:'11'}}>E-mail</FormLabel>
                     
                     <InputGroup>
-                        <InputLeftElement pointerEvents="none" children={<MailIcon />} />
+                        <InputLeftElement w="70px" h="60px" pointerEvents="none" children={<MailIcon stroke="#6E7191" fill="none" width="20"/>} />
 
-                        <Input h="50" focusBorderColor="purple.600" bgColor="gray.400" variant="filled" _hover={ {bgColor: 'gray.100'} } size="lg" borderRadius="20"/>
+                        <Input h="60px" pl="60px" pt="8px" fontSize="sm" focusBorderColor="purple.600" bgColor="gray.400" variant="filled" _hover={ {bgColor: 'gray.100'} } size="lg" borderRadius="24"/>
                     </InputGroup>
                 </FormControl>
                     
-            </Flex>
+            </Stack>
         </Flex>
     )
 }
