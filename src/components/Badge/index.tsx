d8bafe3e71e6615299c6f290ec101d8ce001edaf
Mateus Berlitz <wobetoberlitz@gmail.com>
2021-10-25 08:43:58 -0300

    Badge component change

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from 'react';
-import { Text, Badge as ChakraBadge } from "@chakra-ui/react";
+import { Text, Badge as ChakraBadge, ChakraProps } from "@chakra-ui/react";
 
-interface BadgeProps{
+interface BadgeProps extends ChakraProps{
     children?: ReactNode
 }
 
-export default function Badge(){
+export default function Badge({children}: BadgeProps){
     return (
         <ChakraBadge colorScheme="purple" color="white" bg="purple.500" display="flex" borderRadius="full" px="5" py="0" h="29px" alignItems="center">
-            <Text>Novo!</Text>
+            {children}
         </ChakraBadge>
     )
 }
