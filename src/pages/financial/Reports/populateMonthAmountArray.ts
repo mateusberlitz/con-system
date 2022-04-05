d00f8ef90b6f28138bd9cb55ba4f9df5a03f6655
Mateus Berlitz <wobetoberlitz@gmail.com>
2022-04-05 13:52:17 -0300

    wtf, bug in financial path

--- src/pages/financial/Reports/populateMonthAmountArray.ts
+++
@@ -1,9 +0,0 @@
-export const newMonthsAmountArray = () => {
-    const monthsAmountArray = [];
-
-    for(let m = 1; m <= 13; m++){
-        monthsAmountArray[m] = 0;
-    }
-
-    return monthsAmountArray;
-}
--- src/pages/financial/Reports/populateMonthAmountArraySimple.ts
+++
@@ -1,9 +0,0 @@
-export function newMonthsAmountArraySimple() {
-    const monthsAmountArray = [];
-
-    for (let m = 1; m <= 12; m++) {
-        monthsAmountArray[m] = 0;
-    }
-
-    return monthsAmountArray;
-}
